f3e7153e34861df12dcea12b89e77314bede7524
rojinaAmatya <rojinaamatya1432@gmail.com>
2022-02-13 14:05:13 -0600

    modifications for buss price

--- src/com/drs/dao/impl/BussDAOImpl.java
+++ src/com/drs/dao/impl/BussDAOImpl.java
@@ -25,8 +25,10 @@ public class BussDAOImpl implements BussDAO {
             String destination_from = sc.next();
             System.out.println("Please enter *To* destination:");
             String destination_to = sc.next();
+            System.out.println("Please enter the price of bus ticket: ");
+            double buss_price = sc.nextDouble();
 
-            Buss buss = new Buss(buss_id,buss_name,buss_type, destination_from,destination_to);
+            Buss buss = new Buss(buss_id,buss_name,buss_type, destination_from,destination_to,buss_price);
             addBusses.add(buss);
             System.out.println("Buss information is added successfully");
 
@@ -55,7 +57,7 @@ public class BussDAOImpl implements BussDAO {
     public void updateBuss(int buss_id) {
         for(Buss buss: addBusses){
             if(buss.getBuss_id()== buss_id ){
-                System.out.println("Do you want to update 1. From Destination or 2. To Destination");
+                System.out.println("Do you want to update 1. From Destination or 2. To Destination 3. Price of ticket");
                 int choice = sc.nextInt();
                 switch(choice){
                     case 1:
@@ -70,10 +72,18 @@ public class BussDAOImpl implements BussDAO {
                         System.out.println("To destination is changed successfully");
                         break;
 
+                    case 3:
+                        System.out.println("Please enter the price to update: ");
+                        buss.setBuss_price(sc.nextDouble());
+                        System.out.println("The price is updated successfully");
+
                     default:
-                        System.out.println("Please choose between option 1 and 2");
+                        System.out.println("Please choose between option 1, 2,and 3");
                 }//end of switch
             }//end of if
+            else{
+                System.out.println("Buss record is not found!");
+            }// end of else
         }// end of for
 
     }
